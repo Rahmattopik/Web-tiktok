9b7545387b1ae5acc7a6a653b398c285180cbf46
rahmattopik <kerena093@gmail.com>
2024-06-02 07:33:01 +0700

    buat web tiktok simpel

---
+++ app.js
@@ -0,0 +1,21 @@
+new Vue({
+  el: '#app',
+  data: {
+    livejudul:'jam live',
+    live:'saya akan live jama 21:30 jika saya tidak sibuk. dan akan live minimal 1 jam jangan lupa tonton live saya y',
+    title: 'TAMHAr',
+    subtitle: 'perkenalan singkat saya',
+    description: 'hai saya afilatet pemula. saya menjadi afilatet karna ingin menguji mental dan melancarkan bicara saya. dan saya sedang bekerja dan kuliah. jadi saya menjadi afilate ya karna saya ingin berkembang. ',
+    
+    
+    
+    
+    teamTitle: 'team',
+    team: [
+      { name: 'tamhar', role: 'pembuat vidio' },
+      { name: 'tamhar', role: 'pembuat naskah' },
+      { name: 'tamhar', role: 'pembuat web dari chat gpt' },
+      { name: 'tamhar', role: 'pengedit vidio' }
+    ]
+  }
+});
